6f45eeb72aeac2e6ecdcd31f3df3d75271d70516
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-11 23:39:24 +0500

    Edited to lambda expressions

--- app/src/main/java/com/example/rx/MainActivity.kt
+++ app/src/main/java/com/example/rx/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rx
 
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,12 +8,8 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import com.example.rx.data.API
-import com.example.rx.data.UsersList
-import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -21,47 +18,35 @@ class MainActivity : AppCompatActivity() {
 
     private val BASE_URL = "https://api.github.com/search/"
     private val TAG = "RXTAG"
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET)
-            != PackageManager.PERMISSION_GRANTED
+                != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.INTERNET),
-                1
+                    this,
+                    arrayOf(android.Manifest.permission.INTERNET),
+                    1
             )
         }
 
         val rxTutorialRetrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
 
         val testObservable = rxTutorialRetrofit.create(API::class.java).getUserDetails("rokano")
         testObservable.subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : SingleObserver<Response<UsersList>> {
-                override fun onSuccess(t: Response<UsersList>) {
-                    val top = t.body()?.users
-                    Log.d(TAG, "${top?.size}")
-
-                    //here we can analyze our data or pass it to main thread initialize UI
-                }
-
-                override fun onSubscribe(d: Disposable) {
-                    Log.d(TAG, "onSubscribe called")
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.d(TAG, "Error perfomed")
-                    e.printStackTrace()
-                }
-            })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ Log.d(TAG, "${it.body()?.users?.size}") },
+                        {
+                            it.printStackTrace()
+                        })
         Log.d(TAG, "Main thread")
     }
 }
